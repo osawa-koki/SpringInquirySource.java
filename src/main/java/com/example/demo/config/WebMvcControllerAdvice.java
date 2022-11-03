9c244a41e50d05fc48e4b58dbff30325426b659b
osawa-koki <osawa-koki@osawa.dev>
2022-11-03 18:07:42 +0900

    例外処理書いた。

--- src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
+++ src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import com.example.demo.service.InquiryNotFoundException;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.ui.Model;
@@ -11,24 +12,21 @@ import org.springframework.web.bind.annotation.InitBinder;
 @ControllerAdvice
 public class WebMvcControllerAdvice {
 
-	/*
-	 * This method changes empty character to null
-	 */
-    @InitBinder
-    public void initBinder(WebDataBinder dataBinder) {
-        dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
-    }
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+			dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+	}
     
 	@ExceptionHandler(EmptyResultDataAccessException.class)
-	public String handleException(EmptyResultDataAccessException e,Model model) {
+	public String handleException(EmptyResultDataAccessException e, Model model) {
 		model.addAttribute("message", e);
 		return "error/CustomPage";
 	}
 	
-//	@ExceptionHandler(InquiryNotFoundException.class)
-//	public String handleException(InquiryNotFoundException e,Model model) {
-//		model.addAttribute("message", e);
-//		return "error/CustomPage";
-//	}
+	@ExceptionHandler(InquiryNotFoundException.class)
+	public String handleException(InquiryNotFoundException e, Model model) {
+		model.addAttribute("message", e);
+		return "error/CustomPage";
+	}
    
 }
