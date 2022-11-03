2c643ecf70fafdbe10459da56c7fd0e49a33b71d
osawa-koki <osawa-koki@osawa.dev>
2022-11-03 11:58:45 +0900

    データベースとかの設定まで。

--- src/main/java/com/example/demo/app/SampleController.java
+++ src/main/java/com/example/demo/app/SampleController.java
@@ -1,13 +1,12 @@
 package com.example.demo.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-/*
- * Add annotations here
- */
+import java.util.Map;
 
 
 // コントローラクラスとして宣言
@@ -16,20 +15,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample")
 public class SampleController {
 	
-// 	private final JdbcTemplate jdbcTemplate;
+	private final JdbcTemplate jdbcTemplate;
 
-// 	//Add an annotation here 
-// 	public SampleController(JdbcTemplate jdbcTemplate) {
-// 		this.jdbcTemplate = jdbcTemplate;
-// 	}
+	public SampleController(JdbcTemplate jdbcTemplate) {
+	this.jdbcTemplate = jdbcTemplate;
+}
 
 	// 「/sample/test」へのアクセスに対応
 	@GetMapping("/test")
 	public String index(Model model) {
 		
 		// データ(モデル)をHTMLに渡す
-		model.addAttribute("title", "Inquiry Form");
+		String sql = "";
+		sql += " SELECT id, name, email ";
+		sql += " FROM inquiry ";
+		sql += " WHERE id = 1; ";
+		Map<String, Object> map = jdbcTemplate.queryForMap(sql);
 
+		model.addAttribute("title", "Inquiry");
+		model.addAttribute("name", map.get("name"));
+		model.addAttribute("email", map.get("email"));
+
+		// 「test.html」を対象にレンダリング
 		return "test";
 	}
 
