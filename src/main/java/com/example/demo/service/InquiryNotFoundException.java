7b7fc18464fcefe24c2b4a07d5d7bf7d115829ad
osawa-koki <osawa-koki@osawa.dev>
2022-11-03 18:11:30 +0900

    独自例外作った。

--- src/main/java/com/example/demo/service/InquiryNotFoundException.java
+++ src/main/java/com/example/demo/service/InquiryNotFoundException.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-public class InquiryNotFoundException extends RuntimeException{
+public class InquiryNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
