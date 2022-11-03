c34be51d0e2c36ea8fd5141a319d70d6bf239ae9
osawa-koki <osawa-koki@osawa.dev>
2022-11-03 18:09:51 +0900

    例外処理書いた。

--- src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
+++ src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
@@ -16,7 +16,9 @@ public class WebMvcControllerAdvice {
 	public void initBinder(WebDataBinder dataBinder) {
 			dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
 	}
-    
+
+	// SQLのセレクト結果が一件もなかった場合
+	// data.sqlの「inquiry」に対するINSERT文を削除して、「http://localhost:8080/sample/test」へアクセス。
 	@ExceptionHandler(EmptyResultDataAccessException.class)
 	public String handleException(EmptyResultDataAccessException e, Model model) {
 		model.addAttribute("message", e);
