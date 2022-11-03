86c5492f689ad16e787b946af5a8f685f3f0a693
osawa-koki <osawa-koki@osawa.dev>
2022-11-03 18:26:19 +0900

    ExceptionHandler作った。

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -8,10 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
@@ -114,5 +111,15 @@ public class InquiryController {
 
 		return "redirect:/inquiry/form";
 	}
+
+	// 例外の補足
+	// 例外に関する共通処理(ひとつのハンドラ内)
+	// 「InquiryNotFoundException」が発生したら呼ばれる。
+	@ExceptionHandler(InquiryNotFoundException.class)
+	public String handlerException(InquiryNotFoundException e, Model model) {
+		 model.addAttribute("message", e);
+		 return "error/CustomPage";
+	}
+
 
 }
