96aa7f2fdf73481fb5c67fcfbe8c5be539d5385f
osawa-koki <osawa-koki@osawa.dev>
2022-11-03 16:18:23 +0900

    ミスの修正。

--- src/main/java/com/example/demo/dao/InquiryDaoImpl.java
+++ src/main/java/com/example/demo/dao/InquiryDaoImpl.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
